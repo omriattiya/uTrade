a7a4a6200745bda43149e1d4683caeff5c53f508
Omri <omri206attiya@gmail.com>
2018-04-15 14:19:11 +0300

    acceptance

--- Tests/AcceptanceTests/Bridge.py
+++ Tests/AcceptanceTests/Bridge.py
@@ -1,5 +1,12 @@
+from DatabaseLayer import Owners
+from DomainLayer import ItemsLogic
+from DomainLayer import ShopLogic
+from DomainLayer import ShoppingLogic
 from DomainLayer import UsersLogic
+from SharedClasses.Item import Item
 from SharedClasses.RegisteredUser import RegisteredUser
+from SharedClasses.Shop import Shop
+from SharedClasses.ShoppingCart import ShoppingCart
 
 
 def register_user(username, password):
@@ -7,4 +14,28 @@ def register_user(username, password):
 
 
 def login(username, password):
-    UsersLogic.login(RegisteredUser(username, password))
+    return UsersLogic.login(RegisteredUser(username, password))
+
+
+def open_shop(username, shop_name):
+    return ShopLogic.create_shop(Shop(shop_name, 'ACTIVE'), username)
+
+
+def is_owner(username, shop_name):
+    return Owners.get_owner(username, shop_name) is not False
+
+
+def add_item_to_shop(shop_name, item_name, item_category, keywords, price, quantity, username):
+    return ItemsLogic.add_item_to_shop(Item(0, shop_name, item_name, item_category, keywords, price, quantity),
+                                       username)
+
+
+def buy_item(username, shop_name, item_id, quantity):
+    return ShoppingLogic.add_item_shopping_cart(ShoppingCart(username, item_id, quantity, ""))
+
+
+def is_item_bought(username, item_id):
+    for item in ShoppingLogic.get_cart_items(username):
+        if item.item_id == item_id:
+            return True
+    return False
--- Tests/AcceptanceTests/Customer.py
+++ Tests/AcceptanceTests/Customer.py
@@ -18,6 +18,60 @@ class Customer(unittest.TestCase):
     def login(self):
         Bridge.register_user('SHALOM', '123456')
         is_logged = Bridge.login('SHALOM', '123456')
+        self.assertTrue(is_logged)
+
+    def open_shop(self):
+        # unregistered user
+        is_opened = Bridge.open_shop('username', 'shop_name')
+        self.assertFalse(is_opened)
+        is_owner = Bridge.is_owner('username', 'shop_name')
+        self.assertFalse(is_owner)
+        # registered user
+        Bridge.register_user('username', 'password')
+        Bridge.login('username', 'password')
+        is_opened = Bridge.open_shop('username', 'shop_name')
+        self.assertTrue(is_opened)
+        # check if became owner
+        is_owner = Bridge.is_owner('username', 'shop_name')
+        self.assertTrue(is_owner)
+
+    def buy_items(self):
+        # open shop
+        Bridge.register_user('username', 'password')
+        Bridge.login('username', 'password')
+        Bridge.open_shop('username', 'shop_name')
+        # item is = 1
+        item_added = Bridge.add_item_to_shop(shop_name='shop_name', item_name='item_name1',
+                                             item_category='item_category', keywords='keywords', price=10,
+                                             quantity=100, username='username')
+        self.assertTrue(item_added)
+        # item is = 2
+        item_added = Bridge.add_item_to_shop(shop_name='shop_name', item_name='item_name2',
+                                             item_category='item_category', keywords='keywords', price=10,
+                                             quantity=100, username='username')
+        self.assertTrue(item_added)
+        # item is = 3
+        item_added = Bridge.add_item_to_shop(shop_name='shop_name', item_name='item_name3',
+                                             item_category='item_category', keywords='keywords', price=10,
+                                             quantity=100, username='username')
+        self.assertTrue(item_added)
+
+        Bridge.buy_item(username='username', shop_name='shop_name', item_id=1, quantity=10)
+        self.assertTrue(Bridge.is_item_bought(username='username', item_id=1))
+
+        Bridge.buy_item(username='username', shop_name='shop_name', item_id=2, quantity=10)
+        self.assertTrue(Bridge.is_item_bought(username='username', item_id=2))
+
+        Bridge.buy_item(username='username', shop_name='shop_name', item_id=3, quantity=10)
+        self.assertTrue(Bridge.is_item_bought(username='username', item_id=3))
+
+        Bridge.buy_item(username='username', shop_name='shop_name', item_id=4, quantity=10)
+        self.assertFalse(Bridge.is_item_bought(username='username', item_id=4))
+
+        is_bought = Bridge.buy_item(username='username', shop_name='shop_name', item_id=3, quantity=150)
+        self.assertFalse(is_bought)
+
+    def
 
     def tearDown(self):
         os.remove('db.sqlite3')
