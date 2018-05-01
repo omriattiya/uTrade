10ec38fd4025a368935eda4deb80e9fc800e1dae
Shahar <shahar0897@gmail.com>
2018-05-01 09:53:03 +0300

    - addde GUI routes scripts

--- ServiceLayer/services/PresentationServices/Home.py
+++ ServiceLayer/services/PresentationServices/Home.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 def get_home(request):
     if request.method == 'GET':
-        return render(request,'index.html',context=None)
+        return render(request, 'index.html', context=None)
 
+
+def get_register(request):
+    pass
