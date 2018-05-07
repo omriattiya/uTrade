671c28eca91e178691f3b7b901abb1e7b79e9edb
Omri <omri206attiya@gmail.com>
2018-05-07 18:22:42 +0300

    some fixes

---
+++ PresentationLayer/templates/js/add_to_cart.js
@@ -0,0 +1,32 @@
+function addToCart(item_id) {
+    let data = new FormData();
+    data.append('item_id', item_id);
+    data.append('quantity', 1);
+    let loadHTML = new XMLHttpRequest();
+    loadHTML.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            if (loadHTML.responseText === 'fail') {
+                alert("add to cart fail");
+            }
+            else {
+                if (getCookie('login_hash') === '') {
+                    if (getCookie('guest_hash') === '') {
+                        alert("new guest");
+                        setCookie('guest_hash', loadHTML.responseTEXT, 7);
+                        document.write(loadHTML.responseTEXT);
+                        // location.reload()
+                    }
+                    else {
+                        alert("old guest");
+                        location.reload()
+                    }
+                }
+                else {
+                    alert("user");
+                }
+            }
+        }
+    };
+    loadHTML.open("POST", "http://localhost:8000/app/shopping_cart/add_item_shopping_cart/", true);
+    loadHTML.send(data);
+}
