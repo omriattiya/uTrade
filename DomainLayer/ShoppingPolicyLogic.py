b2cb4d861646650e09884f22426b2c395de363b0
Yoni Tsionov <yonits12@gmail.com>
2018-05-31 18:27:37 +0300

    ShoppingPolicy Logic Extensions ShoppingPolicy Service

--- DomainLayer/ShoppingPolicyLogic.py
+++ DomainLayer/ShoppingPolicyLogic.py
@@ -1,4 +1,6 @@
 from DatabaseLayer import ShoppingPolicies
+from DatabaseLayer import SystemManagers
+from DatabaseLayer import Owners
 
 #    ____________________________________   GET ALL     ___________________________________________________
 
@@ -22,109 +24,157 @@ def get_all_shopping_policy_on_identity():
 #    ____________________________________   INSERT     ___________________________________________________
 
 
-def add_shopping_policy_on_items(item_name, conditions, restrict, quantity):
+def add_shopping_policy_on_items(username, item_name, conditions, restrict, quantity):
     if item_name is not None and conditions is not None:
         if restrict is not None and quantity is not None:
             if quantity < 0:
                 return "FAILED: Negative quantity is invalid."
-            return ShoppingPolicies.add_shopping_policy_on_items(item_name, conditions, restrict, quantity)
+            if SystemManagers.is_system_manager(username) is not False:
+                if not ShoppingPolicies.add_shopping_policy_on_items(item_name, conditions, restrict, quantity):
+                    return "FAILED: DB error."
+                return True
+            return 'FAILED: you are not a System Manager'
         return "FAILED: One (or more) of the parameters is None"
     return "FAILED: One (or more) of the parameters is None"
 
 
-def add_shopping_policy_on_category(category, conditions, restrict, quantity):
+def add_shopping_policy_on_category(username, category, conditions, restrict, quantity):
     if category is not None and conditions is not None:
         if restrict is not None and quantity is not None:
             if quantity < 0:
                 return "FAILED: Negative quantity is invalid."
-            return ShoppingPolicies.add_shopping_policy_on_category(category, conditions, restrict, quantity)
+            if SystemManagers.is_system_manager(username) is not False:
+                if not ShoppingPolicies.add_shopping_policy_on_category(category, conditions, restrict, quantity):
+                    return "FAILED: DB error."
+                return True
+            return 'FAILED: you are not a System Manager'
         return "FAILED: One (or more) of the parameters is None"
     return "FAILED: One (or more) of the parameters is None"
 
 
-def add_shopping_policy_on_shop(shop_name, conditions, restrict, quantity):
+def add_shopping_policy_on_shop(username, shop_name, conditions, restrict, quantity):
     if shop_name is not None and conditions is not None:
         if restrict is not None and quantity is not None:
             if quantity < 0:
                 return "FAILED: Negative quantity is invalid."
-            return ShoppingPolicies.add_shopping_policy_on_shop(shop_name, conditions, restrict, quantity)
+            if Owners.get_owner(username, shop_name) is not False:
+                if not ShoppingPolicies.add_shopping_policy_on_shop(shop_name, conditions, restrict, quantity):
+                    return "FAILED: DB error."
+                return True
+            return 'FAILED: you are not a the Owner of the shop'
         return "FAILED: One (or more) of the parameters is None"
     return "FAILED: One (or more) of the parameters is None"
 
 
-def add_shopping_policy_on_identity(conditions, restrict, quantity):
+def add_shopping_policy_on_identity(username, conditions, restrict, quantity):
     if conditions is not None and restrict is not None and quantity is not None:
         if quantity < 0:
             return "FAILED: Negative quantity is invalid."
-        return ShoppingPolicies.add_shopping_policy_on_identity(conditions, restrict, quantity)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.add_shopping_policy_on_identity(conditions, restrict, quantity):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: One (or more) of the parameters is None"
 
 
 #    ____________________________________   DELETE     ___________________________________________________
 
 
-def remove_shopping_policy_on_identity(policy_id):
+def remove_shopping_policy_on_identity(username, policy_id):
     if policy_id is not None and policy_id > 0:
-        return ShoppingPolicies.remove_shopping_policy_on_identity(policy_id)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.remove_shopping_policy_on_identity(policy_id):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: Invalid id of Policy"
 
 
-def remove_shopping_policy_on_shop(policy_id):
+def remove_shopping_policy_on_shop(username, policy_id, shop_name):
     if policy_id is not None and policy_id > 0:
-        return ShoppingPolicies.remove_shopping_policy_on_shop(policy_id)
+        if Owners.get_owner(username, shop_name) is not False:
+            if not ShoppingPolicies.remove_shopping_policy_on_shop(policy_id):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a the Owner of the shop'
     return "FAILED: Invalid id of Policy"
 
 
-def remove_shopping_policy_on_items(policy_id):
+def remove_shopping_policy_on_items(username, policy_id):
     if policy_id is not None and policy_id > 0:
-        return ShoppingPolicies.remove_shopping_policy_on_items(policy_id)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.remove_shopping_policy_on_items(policy_id):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: Invalid id of Policy"
 
 
-def remove_shopping_policy_on_category(policy_id):
+def remove_shopping_policy_on_category(username, policy_id):
     if policy_id is not None and policy_id > 0:
-        return ShoppingPolicies.remove_shopping_policy_on_category(policy_id)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.remove_shopping_policy_on_category(policy_id):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: Invalid id of Policy"
 
 
 #    ____________________________________   UPDATE     ___________________________________________________
 
 
-def update_shopping_policy_on_identity(policy_id, field_name, new_value):
+def update_shopping_policy_on_identity(username, policy_id, field_name, new_value):
     if policy_id is not None and field_name is not None and new_value is not None:
         if policy_id < 0:
             return "FAILED: Invalid id of Policy"
         if field_name not in ['conditions', 'restrict', 'quantity']:
             return "FAILED: Invalid field name"
-        return ShoppingPolicies.update_shopping_policy_on_identity(policy_id, field_name, new_value)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.update_shopping_policy_on_identity(policy_id, field_name, new_value):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: One (or more) of the parameters is None"
 
 
-def update_shopping_policy_on_shop(policy_id, field_name, new_value):
+def update_shopping_policy_on_shop(username, policy_id, field_name, new_value, shop_name):
     if policy_id is not None and field_name is not None and new_value is not None:
         if policy_id < 0:
             return "FAILED: Invalid id of Policy"
         if field_name not in ['shop_name', 'conditions', 'restrict', 'quantity']:
             return "FAILED: Invalid field name"
-        return ShoppingPolicies.update_shopping_policy_on_shop(policy_id, field_name, new_value)
+        if Owners.get_owner(username, shop_name) is not False:
+            if not ShoppingPolicies.update_shopping_policy_on_shop(policy_id, field_name, new_value):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a the Owner of the shop'
     return "FAILED: One (or more) of the parameters is None"
 
 
-def update_shopping_policy_on_items(policy_id, field_name, new_value):
+def update_shopping_policy_on_items(username, policy_id, field_name, new_value):
     if policy_id is not None and field_name is not None and new_value is not None:
         if policy_id < 0:
             return "FAILED: Invalid id of Policy"
         if field_name not in ['item_name', 'conditions', 'restrict', 'quantity']:
             return "FAILED: Invalid field name"
-        return ShoppingPolicies.update_shopping_policy_on_items(policy_id, field_name, new_value)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.update_shopping_policy_on_items(policy_id, field_name, new_value):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: One (or more) of the parameters is None"
 
 
-def update_shopping_policy_on_category(policy_id, field_name, new_value):
+def update_shopping_policy_on_category(username, policy_id, field_name, new_value):
     if policy_id is not None and field_name is not None and new_value is not None:
         if policy_id < 0:
             return "FAILED: Invalid id of Policy"
         if field_name not in ['category', 'conditions', 'restrict', 'quantity']:
             return "FAILED: Invalid field name"
-        return ShoppingPolicies.update_shopping_policy_on_category(policy_id, field_name, new_value)
+        if SystemManagers.is_system_manager(username) is not False:
+            if not ShoppingPolicies.update_shopping_policy_on_category(policy_id, field_name, new_value):
+                return "FAILED: DB error."
+            return True
+        return 'FAILED: you are not a System Manager'
     return "FAILED: One (or more) of the parameters is None"
---
+++ ServiceLayer/services/LogicServices/ShoppingPolicyService.py
@@ -0,0 +1,264 @@
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from DomainLayer import ShoppingPolicyLogic
+from ServiceLayer.services.LiveAlerts import Consumer
+
+
+#    ____________________________________   GET ALL     ___________________________________________________
+
+
+def get_all_shopping_policy_on_shop(request):
+    if request.method == 'GET':
+        return ShoppingPolicyLogic.get_all_shopping_policy_on_shop()
+    return HttpResponse('FAILED')
+
+
+def get_all_shopping_policy_on_items(request):
+    if request.method == 'GET':
+        return ShoppingPolicyLogic.get_all_shopping_policy_on_items()
+    return HttpResponse('FAILED')
+
+
+def get_all_shopping_policy_on_identity(request):
+    if request.method == 'GET':
+        return ShoppingPolicyLogic.get_all_shopping_policy_on_identity()
+    return HttpResponse('FAILED')
+
+
+def get_all_shopping_policy_on_category(request):
+    if request.method == 'GET':
+        return ShoppingPolicyLogic.get_all_shopping_policy_on_category()
+    return HttpResponse('FAILED')
+
+
+#    ____________________________________   INSERT     ___________________________________________________
+
+@csrf_exempt
+def add_shopping_policy_on_items(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            item_name = request.POST.get('item_name')
+            conditions = request.POST.get('conditions')
+            restrict = request.POST.get('restrict')
+            quantity = request.POST.get('quantity')
+            status = ShoppingPolicyLogic.add_shopping_policy_on_items(username, item_name, conditions, restrict, quantity)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def add_shopping_policy_on_category(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            category = request.POST.get('category')
+            conditions = request.POST.get('conditions')
+            restrict = request.POST.get('restrict')
+            quantity = request.POST.get('quantity')
+            status = ShoppingPolicyLogic.add_shopping_policy_on_category(username, category, conditions, restrict, quantity)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def add_shopping_policy_on_shop(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            shop_name = request.POST.get('shop_name')
+            conditions = request.POST.get('conditions')
+            restrict = request.POST.get('restrict')
+            quantity = request.POST.get('quantity')
+            status = ShoppingPolicyLogic.add_shopping_policy_on_shop(username, shop_name, conditions, restrict, quantity)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def add_shopping_policy_on_identity(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            conditions = request.POST.get('conditions')
+            restrict = request.POST.get('restrict')
+            quantity = request.POST.get('quantity')
+            status = ShoppingPolicyLogic.add_shopping_policy_on_identity(username, conditions, restrict, quantity)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+#    ____________________________________   DELETE     ___________________________________________________
+
+@csrf_exempt
+def remove_shopping_policy_on_identity(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            status = ShoppingPolicyLogic.remove_shopping_policy_on_identity(username, policy_id)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def remove_shopping_policy_on_shop(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            shop_name = request.POST.get('shop_name')
+            status = ShoppingPolicyLogic.remove_shopping_policy_on_shop(username, policy_id, shop_name)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def remove_shopping_policy_on_items(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            status = ShoppingPolicyLogic.remove_shopping_policy_on_items(username, policy_id)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def remove_shopping_policy_on_category(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            status = ShoppingPolicyLogic.remove_shopping_policy_on_category(username, policy_id)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+#    ____________________________________   UPDATE     ___________________________________________________
+
+
+@csrf_exempt
+def update_shopping_policy_on_identity(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            field_name = request.POST.get('field_name')
+            new_value = request.POST.get('new_value')
+            status = ShoppingPolicyLogic.update_shopping_policy_on_identity(username, policy_id, field_name, new_value)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def update_shopping_policy_on_shop(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            field_name = request.POST.get('field_name')
+            new_value = request.POST.get('new_value')
+            shop_name = request.POST.get('shop_name')
+            status = ShoppingPolicyLogic.update_shopping_policy_on_shop(username, policy_id, field_name, new_value, shop_name)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def update_shopping_policy_on_items(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            field_name = request.POST.get('field_name')
+            new_value = request.POST.get('new_value')
+            status = ShoppingPolicyLogic.update_shopping_policy_on_items(username, policy_id, field_name, new_value)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
+
+
+@csrf_exempt
+def update_shopping_policy_on_category(request):
+    if request.method == 'POST':
+        login = request.COOKIES.get('login_hash')
+        if login is not None:
+            username = Consumer.loggedInUsers.get(login)
+            if username is None:
+                return HttpResponse('FAILED: username is None')
+            policy_id = request.POST.get('policy_id')
+            field_name = request.POST.get('field_name')
+            new_value = request.POST.get('new_value')
+            status = ShoppingPolicyLogic.update_shopping_policy_on_category(username, policy_id, field_name, new_value)
+            if status is not True:
+                return HttpResponse(status)
+            return HttpResponse('SUCCESS')
+        return HttpResponse('FAILED: you are not logged in!')
+    return HttpResponse('FAILED: not a POST request')
