0e82d9c2e90add25801a9ed7e4deb564ac2a5cbe
TomerLev <levtom@post.bgu.ac.il>
2018-06-01 13:07:48 +0300

    Another Fixes.

--- PresentationLayer/templates/js/filterNew.js
+++ PresentationLayer/templates/js/filterNew.js
@@ -21,10 +21,27 @@
             for (i = 0; i < x.length; i++)
             {
                 var attributesOfItems = x[i].attributes;
-                var priceOfItem = attributesOfItems[1].value;
-                var startPrice = document.getElementById("SearchForm6").value;
-                var endPrice = document.getElementById("SearchForm7").value;
+                var priceOfItem = parseFloat(attributesOfItems[1].value);
+                var startPrice = parseFloat(document.getElementById("SearchForm6").value);
+                var endPrice = parseFloat(document.getElementById("SearchForm7").value);
                 if(!((startPrice <= priceOfItem) && (priceOfItem <= endPrice)))
+                {
+                    x[i].hidden = true;
+                }
+            }
+        }
+
+        function filterByItemRanking()
+        {
+            var x = document.getElementsByClassName("itemJS");
+            var i;
+            for (i = 0; i < x.length; i++)
+            {
+                var attributesOfItems = x[i].attributes;
+                var rankingOfItem = parseFloat(attributesOfItems[3].value);
+                var startRank = parseFloat(document.getElementById("SearchForm8").value);
+                var endRank = parseFloat(document.getElementById("SearchForm9").value);
+                if(!((startRank <= rankingOfItem) && (rankingOfItem <= endRank)))
                 {
                     x[i].hidden = true;
                 }
