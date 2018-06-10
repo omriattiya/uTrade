ae72e9cb514772d5adbdcf4f15a2a6e12ba7dd24
Yoni Tsionov <yonits12@gmail.com>
2018-06-11 01:40:38 +0300

    Show Last price & after Discount Category Discount Showing Lottery vs. Discount Table Design

--- ServiceLayer/services/PresentationServices/Item.py
+++ ServiceLayer/services/PresentationServices/Item.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 from DatabaseLayer import Lotteries
 from DomainLayer import ItemsLogic
-from DomainLayer.DiscountLogic import get_visible_discount
+from DomainLayer.DiscountLogic import get_visible_discount, get_visible_discount_category
 from ServiceLayer.services.PresentationServices import Topbar_Navbar
 
 shop_not_exist = 'item does not exist'
@@ -22,13 +22,21 @@ def get_item(request):
             #                                   {'name': item.name, 'price': item.price, 'url': item.url}, None,
             #                                  None)
             right1 = "Percentage"
-            policy_or_percentage = "Immediately"
+            policy_or_percentage = "0"
+            item_percentage = "0"
             right2 = "Start Date"
             deadline_or_start_date = "None"
+            item_start_date = "None"
+            item_end_date = "None"
             right3 = "End Time"
             real_end_time_or_end_date = "None"
             headline = "Purchase Policy"
+            price = item.price
+            former_price = ""
             lottery = Lotteries.get_lottery(item_id)
+            invisible = ""
+            header_of_table = "Category Discount"
+            lottery_margin_left = 0
             quantity_icon = 'icon-inventory.png'
             if lottery is not False:
                 right1 = "Policy"
@@ -36,20 +44,31 @@ def get_item(request):
                 print(lottery.final_date)
                 right2 = "Deadline"
                 deadline_or_start_date = lottery.final_date
-                right3 = "Actual Rnd Time"
+                right3 = "Actual End Time"
+                invisible = "display: none;"
+                header_of_table = "Additional details you should know before purchase"
+                lottery_margin_left = 30
                 if lottery.real_end_date is not None:
                     real_end_time_or_end_date = lottery.real_end_date
                 else:
                     real_end_time_or_end_date = "---------"
                 quantity_icon = 'tickets-icon.png'
             else:
-                headline = "Discount on Product"
+                headline = "Discounts on Product"
                 discount = get_visible_discount(item.id, item.shop_name)
                 if discount is not False:
+                    item_start_date = discount.from_date
+                    item_percentage = str(discount.percentage) + "%"
+                    item_end_date = discount.end_date
+                    former_price = "$" + str(item.price)
+                    price = price*(1 - (discount.percentage / 100))
+                discount = get_visible_discount_category(item.category, item.shop_name)
+                if discount is not False:
                     deadline_or_start_date = discount.from_date
-                    policy_or_percentage = discount.percentage
+                    policy_or_percentage = str(discount.percentage) + "%"
                     real_end_time_or_end_date = discount.end_date
-
+                    former_price = "$" + str(item.price)
+                    price = price * (1 - (discount.percentage / 100))
             login = request.COOKIES.get('login_hash')
             guest = request.COOKIES.get('guest_hash')
             context = {'topbar': Topbar_Navbar.get_top_bar(login), 'navbar': Topbar_Navbar.get_nav_bar(login, guest)}
@@ -63,7 +82,8 @@ def get_item(request):
                             'shop_name': item.shop_name,
                             'category': item.category,
                             'keyWords': item.keyWords,
-                            'price': item.price,
+                            'price': round(price, 2),
+                            'former_price': former_price,
                             'quantity': item.quantity,
                             'kind': item.kind,
                             'item_rank': item_rank,
@@ -75,6 +95,12 @@ def get_item(request):
                             'right1': right1,
                             'right2': right2,
                             'right3': right3,
+                            'header_of_table': header_of_table,
+                            'invisible': invisible,
+                            'lottery_margin_left': lottery_margin_left,
+                            'item_percentage': item_percentage,
+                            'item_start_date': item_start_date,
+                            'item_end_date': item_end_date,
                             'quantity_icon': quantity_icon})
             return render(request, 'detail.html', context=context)
         else:
